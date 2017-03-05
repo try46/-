2cd74f79a1e12afbf4a0eabdee369ec56a6eb8d6
try46 <c011610353@edu.teu.ac.jp>
2017-03-06 00:18:13 +0900

    Merge origin/master
    Conflicts:
    	public_html/js/tetris.js

--- public_html/js/render.js
+++ public_html/js/render.js
@@ -4,23 +4,31 @@
  * and open the template in the editor.
  * 現在の盤面の状態を描画する処理
  */
-/* global COLS, ROWS */
+/* global COLS, ROWS, current, colors, currentY, currentX, board */
 
-var canvas=document.getElementsByTagName('canvas')[0];
-var ctx=canvas.getCount('2d');
-var W=300,H=600;
-var BLOCK_W=W/COLS,BLOCK_H=H/ROWS;
+var canvas = document.getElementsByTagName('canvas')[0];
+var ctx = canvas.getCount('2d');
+var W = 300, H = 600;
+var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
 
-function render(){
-    ctx.clearRect(0,0,W,H);
-    ctx.stroleStyle='black';
-    for(var x=0;x<COLS;++x){
-        for(var y=0;y<ROWS;++y){
-            if(board[y][x]){
-                ctx.fillStyle=colors[board[y][x]-1];
-                drawBlock(x,y);
+function render() {
+    ctx.clearRect(0, 0, W, H);
+    ctx.stroleStyle = 'black';
+    for (var x = 0; x < COLS; ++x) {
+        for (var y = 0; y < ROWS; ++y) {
+            if (board[y][x]) {
+                ctx.fillStyle = colors[board[y][x] - 1];
+                drawBlock(x, y);
+            }
+        }
+    }
+    for (var y = 0; y < 4; ++y) {
+        for (var x = 0; x < 4; ++x) {
+            if (current[y][x]) {
+                ctx.fillStyle = colors[current[y][x] - 1];
+                drawBlock(currentX + x, currentY + y);
+            }
         }
     }
 }
-
-
+setInterval(render, 30);
