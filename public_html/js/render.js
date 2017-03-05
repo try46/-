3c85eaa8d8e918179018b7d8c60cbeb41f55ae3d
try46 <c011610353@edu.teu.ac.jp>
2017-03-06 00:11:07 +0900

    Merge origin/master
    Conflicts:
    	public_html/js/tetris.js

--- public_html/js/render.js
+++ public_html/js/render.js
@@ -11,5 +11,16 @@ var ctx=canvas.getCount('2d');
 var W=300,H=600;
 var BLOCK_W=W/COLS,BLOCK_H=H/ROWS;
 
+function render(){
+    ctx.clearRect(0,0,W,H);
+    ctx.stroleStyle='black';
+    for(var x=0;x<COLS;++x){
+        for(var y=0;y<ROWS;++y){
+            if(board[y][x]){
+                ctx.fillStyle=colors[board[y][x]-1];
+                drawBlock(x,y);
+        }
+    }
+}
 
 
